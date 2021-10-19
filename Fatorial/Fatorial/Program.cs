be57e28d9015c6bb133ee4df30f1541eb64a91d1
SakataWolf <87884401+SakataWolf@users.noreply.github.com>
2021-10-19 17:31:22 -0300

    Add files via upload

---
+++ Fatorial/Fatorial/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Fatorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nFatorial");
+            double resultado = 1;
+            int numero;
+            Console.Write("\n\nDigite um número: ");
+            numero = int.Parse(Console.ReadLine());
+            while (numero != 1)
+            {
+                resultado = resultado * numero;
+                numero = numero - 1;
+            }
+            Console.WriteLine("\n\nO fatorial do número escolhido é "+resultado);
+        }
+    }
+}
